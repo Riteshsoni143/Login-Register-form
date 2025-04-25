ea3f092d41c2aa24b68215583aa7acaf5b787b70
Riteshsoni143 <riteshsoni35468@gmail.com>
2025-04-25 10:41:45 +0530

    modified

--- script.js
+++ script.js
@@ -8,21 +8,20 @@ function showLoginCard() {
   
     let card = document.getElementById("myElement");
     card.style.display = "flex";
+    card.style.height = "310px";
 
     let loginCard = document.getElementById("login_card");
     loginCard.style.transform = "translateX(0px)"
 
     let registerCard = document.getElementById("register_card");
     registerCard.style.transform = "translateX(400px)";
-
-    card.style.height = "290px";
 }
 
 
 function showRegisterCard() {
     let card = document.getElementById("myElement");
     // card.style.display = "flex";
-    card.style.height = "350px";
+
 
     let registerCard = document.getElementById("register_card");
     registerCard.style.transform = "translateX(0px)";
